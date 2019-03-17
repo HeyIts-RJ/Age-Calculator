f4343f3d750cec6baa8588d72b4e99ce9be612ff
HeyIts-RJ <rishab1300@gmail.com>
2019-03-17 14:59:06 +0530

    Added 3 ComboBox

--- src/GUI.java
+++ src/GUI.java
@@ -1,28 +1,59 @@
-import java.awt.FlowLayout;
+import java.awt.Component;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
-import javax.swing.*;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+
 public class GUI {
 
-	GUI(){
-	
-		JFrame f=new JFrame("MY app");
-		int[] dates;
-		dates=new int[31];
-		for(int i=1;i<=31;++i)
-		{
-			dates[i]=i;
-		}
-		JComboBox<Integer> cb=new JComboBox<Integer>();
-		cb.setModel(dates);
-		cb.setBounds(50,50,90,20);
-		f.setLayout(new FlowLayout());
-		f.add(cb);
-		f.setSize(400,400);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
-	}
-	public static void main(String... a)
-	{
-		GUI g=new GUI();
-	}
+    // Create Date Renderer for formatting Date
+    public static class DateComboBoxRenderer extends DefaultListCellRenderer {
+
+        // desired format for the date
+        private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        public Component getListCellRendererComponent( JList list, Object value, int index, boolean isSelected, boolean cellHasFocus ) {
+            Object item = value;
+
+            // if the item to be rendered is date then format it
+            if( item instanceof Date ) {
+                item = dateFormat.format( ( Date ) item );
+            }
+            return super.getListCellRendererComponent( list, item, index, isSelected, cellHasFocus);
+        }
+    }
+
+    public static void main( String[] str ) {
+        JComboBox combo = new JComboBox();
+
+        // Add current date
+        GregorianCalendar calendar = new GregorianCalendar();
+        combo.addItem( calendar.getTime() );
+
+        // Add Next date
+        calendar.roll( GregorianCalendar.DAY_OF_MONTH, 1 );
+        combo.addItem( calendar.getTime() );
+
+        // Set Renderer for formating the date in combobox
+        combo.setRenderer( new DateComboBoxRenderer() );
+
+        JFrame frame = new JFrame( "Date Rendere Example" );
+
+        JPanel panel = new JPanel();
+        panel.add( new JLabel( "Date Combo: ") );
+        panel.add( combo );
+        frame.add( panel );
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible( true );
+    }
+
 }
--- src/agecalc.java
+++ src/agecalc.java
@@ -9,6 +9,9 @@ import javax.swing.JScrollBar;
 import java.awt.GridBagConstraints;
 import javax.swing.JSlider;
 import java.awt.Insets;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class agecalc extends JFrame {
 
@@ -40,17 +43,60 @@ public class agecalc extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JSlider slider = new JSlider();
-		GridBagConstraints gbc_slider = new GridBagConstraints();
-		gbc_slider.gridx = 4;
-		gbc_slider.gridy = 1;
-		contentPane.add(slider, gbc_slider);
+		JLabel lblDate = new JLabel("Date");
+		GridBagConstraints gbc_lblDate = new GridBagConstraints();
+		gbc_lblDate.anchor = GridBagConstraints.NORTHEAST;
+		gbc_lblDate.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDate.gridx = 3;
+		gbc_lblDate.gridy = 3;
+		contentPane.add(lblDate, gbc_lblDate);
+		
+		JComboBox comboBox = new JComboBox(dates);
+		comboBox.setToolTipText("");
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 4;
+		gbc_comboBox.gridy = 3;
+		contentPane.add(comboBox, gbc_comboBox);
+		
+		JLabel lblMonth = new JLabel("Month");
+		GridBagConstraints gbc_lblMonth = new GridBagConstraints();
+		gbc_lblMonth.anchor = GridBagConstraints.EAST;
+		gbc_lblMonth.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMonth.gridx = 3;
+		gbc_lblMonth.gridy = 4;
+		contentPane.add(lblMonth, gbc_lblMonth);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.gridx = 4;
+		gbc_comboBox_1.gridy = 4;
+		contentPane.add(comboBox_1, gbc_comboBox_1);
+		
+		JLabel lblYear = new JLabel("Year");
+		GridBagConstraints gbc_lblYear = new GridBagConstraints();
+		gbc_lblYear.anchor = GridBagConstraints.EAST;
+		gbc_lblYear.insets = new Insets(0, 0, 0, 5);
+		gbc_lblYear.gridx = 3;
+		gbc_lblYear.gridy = 5;
+		contentPane.add(lblYear, gbc_lblYear);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		GridBagConstraints gbc_comboBox_2 = new GridBagConstraints();
+		gbc_comboBox_2.insets = new Insets(0, 0, 0, 5);
+		gbc_comboBox_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_2.gridx = 4;
+		gbc_comboBox_2.gridy = 5;
+		contentPane.add(comboBox_2, gbc_comboBox_2);
 	}
 
 }
